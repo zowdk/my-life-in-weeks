004664090579fd3de8b7df48310616d50c6642d2
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-07-09 00:03:57 +0200

    set up weeks grid

---
+++ index.js
@@ -0,0 +1,18 @@
+const width = 52;
+const grid = document.querySelector(".grid");
+
+let squares = [];
+
+// 52 * 100 = 5200
+// 0 - week passed
+// 1 - week open
+// 2 - accomplishments
+// 3 - in school
+// 4 - working
+// 5 - training gymnastics
+// 6 - learning french
+// 7 - learning danish
+// 8 - learning coding
+// 9 - living in US
+// 9b - living in CA
+// 9c - living in Denmark
