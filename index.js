bd7a6e801ec5af3d9f5b87a65bad791666fe3b22
Zoe Olivier <80650207+zowdk@users.noreply.github.com>
2022-07-09 00:37:09 +0200

    generate 5200 squares

--- index.js
+++ index.js
@@ -17,25 +17,33 @@ let squares = [];
 // 9b - living in CA
 // 9c - living in Denmark
 
-const layout = [];
+// const layout = [];
+// a map of classes (keys) and functions as values
+// for each class/function pair, set the class on the square
+// if the function returns true for fn(i)
+const states = {
+  "week-spent": (i) => i < 500,
+};
 
 //create grid
 function createGrid() {
   squares = [];
   grid.innerHTML = "";
-  for (i = 0; i < layout.length; i++) {
+  for (i = 0; i < 5200; i++) {
     //create a square
     const square = document.createElement("div");
     //put square into grid
     grid.appendChild(square);
     //put square in squares array
     squares.push(square);
-
-    if (layout[i] === 0) {
-      squares[i].classList.add("week-spent");
-    } else if (layout[i] === 1) {
-      squares[1].classList.add("week-open");
-    }
+    squares[i].classList.add("week-spent");
   }
+
+  //     if (layout[i] === 0) {
+  //       squares[i].classList.add("week-spent");
+  //     } else if (layout[i] === 1) {
+  //       squares[1].classList.add("week-open");
+  //     }
+  //   }
 }
 createGrid();
